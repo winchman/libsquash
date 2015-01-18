45c756b0b98801ffbbb6189d5d3d3c37f40c2cad
Rafe Colton <rafael.colton@gmail.com>
2015-01-18 15:49:00 -0800

    Adding comments to make the linter happy

--- tarball/stream.go
+++ tarball/stream.go
@@ -5,9 +5,22 @@ import (
 	"io"
 )
 
+// Tarstream is a type for writing tarballs
 type Tarstream interface {
+	// Close closes the underlying tar writer
 	Close() error
+
+	// Add adds tf to the underlying tar writer. First, the header is written.
+	// Then, if tf.Stream is not nil, its contents are copied into underlying
+	// tar writer
 	Add(tf *TarFile) error
+}
+
+// NewTarstream returns a tar stream that wries Add()'ed files to outstream
+func NewTarstream(outstream io.Writer) Tarstream {
+	return &tarstream{
+		writer: tar.NewWriter(outstream),
+	}
 }
 
 type tarstream struct {
@@ -26,10 +39,4 @@ func (t *tarstream) Add(tf *TarFile) (err error) {
 		_, err = io.Copy(t.writer, tf.Stream)
 	}
 	return
-}
-
-func NewTarstream(outstream io.Writer) Tarstream {
-	return &tarstream{
-		writer: tar.NewWriter(outstream),
-	}
 }
